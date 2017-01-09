c4efa5c2e2400315228cc8c43970341f8a56a28f
nijianqi <1198952901@qq.com>
2017-01-09 13:32:15 +0800

    公众号消息

--- application/index/model/MessageModel.php
+++ application/index/model/MessageModel.php
@@ -17,57 +17,67 @@ class MessageModel extends BaseModel
         $param['message_type'] = $message_type;
         $param['act_id'] = $act_id;
         $param['topic_id'] = $topic_id;
-		$param['comment_id'] = $comment_id;
+        $param['comment_id'] = $comment_id;
         $param['create_time'] = time();
         $flag = $this->insertGetId($param);
         if(!empty($flag)){
-            $memberModel = new Member();
+            $memberModel = new MemberModel();
             $memberInfo = $memberModel->getInfoById($toMemberId);
-            $url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=ACCESS_TOKEN';
-            $post_data['touser']       = $memberInfo['member_openid'];
-            $post_data['template_id']      = 'B0TldCRq2ljtRcaPMIsQlJJQAmjhscoUOtwPxmFfrec';
-            $post_data['url'] = 'https://www.dlgx888.com/index/member/index.html';
-            $data['first'] = array('value'=>'您有一条消息，请查收','color'=>'#173177');
-            if(!empty($comment_id)){
-                $data['keyword1'] = array('value'=>'评论消息','color'=>'#173177');
-            }else{
-                $data['keyword1'] = array('value'=>'普通消息','color'=>'#173177');
+			$appId = 'wxd53d2b1ef188dca7';//大乐个学
+            $secret = 'aafdb067ff2aef548c50541392cf44b8';
+			$url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $appId . '&secret=' . $secret . '';
+            $result = $this->get_url_contents($url);
+			 if (strpos($result, 'errcode') != FALSE) {
+                $msg = json_decode($result);
+                if (isset($msg->errcode)) {
+                    $this->error('error:' . $msg->errcode);
+                    $this->error('msg:' . $msg->errmsg);
+                    exit;
+                }
             }
-            $data['keyword2'] = array('value'=> time(),'color'=>'#173177');
-            $data['remark'] = array('value'=> $message_content ,'color'=>'#173177');
-            $post_data['data']    = $data;
-            $o = "";
-            foreach ( $post_data as $k => $v )
-            {
-                $o.= "$k=" . urlencode( $v ). "&" ;
-            }
-            $post_data = substr($o,0,-1);
-
-            $this->request_post($url, $post_data);
+            $params = json_decode($result);
+            $url = 'https://api.weixin.qq.com/cgi-bin/message/template/send?access_token='.$params->access_token;
+            $data = array(
+                    'touser' => $memberInfo['member_openid'], // openid是发送消息的基础
+                    'template_id' => 'B0TldCRq2ljtRcaPMIsQlJJQAmjhscoUOtwPxmFfrec', // 模板id
+                    'url' => 'https://www.dlgx888.com/index/member/index.html', // 点击跳转地址
+                    'data' => array(
+                        'first' => array('value' => '您有一条消息，请查收'),
+                        'keyword1' => array('value' => '新的消息'),
+                        'keyword2' => array('value' => date('Y年m月d日 H:i', time())),
+                        'remark' => array('value' => $message_content),
+                    )
+                );
+				print_r(json_encode($data));exit;
+           $this->http_post_data($url, json_encode($data));
         }
 
     }
-    /**
-     * 模拟post进行url请求
-     * @param string $url
-     * @param string $param
-     */
-    function request_post($url = '', $param = '') {
-        if (empty($url) || empty($param)) {
-            return false;
-        }
-
-        $postUrl = $url;
-        $curlPost = $param;
-        $ch = curl_init();//初始化curl
-        curl_setopt($ch, CURLOPT_URL,$postUrl);//抓取指定网页
-        curl_setopt($ch, CURLOPT_HEADER, 0);//设置header
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);//要求结果为字符串且输出到屏幕上
-        curl_setopt($ch, CURLOPT_POST, 1);//post提交方式
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $curlPost);
-        $data = curl_exec($ch);//运行curl
+function http_post_data($url, $data_string) {  
+  
+        $ch = curl_init();  
+        curl_setopt($ch, CURLOPT_POST, 1);  
+        curl_setopt($ch, CURLOPT_URL, $url);  
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);  
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
+            'Content-Type: application/json; charset=utf-8',  
+            'Content-Length: ' . strlen($data_string))  
+        );  
+        ob_start();  
+        curl_exec($ch);  
+        $return_content = ob_get_contents();  
+        ob_end_clean();   
+    }  
+	function get_url_contents($url)
+    {
+        if (ini_get('allow_url_fopen') == 1) return file_get_contents($url);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $result = curl_exec($ch);
         curl_close($ch);
-
-        return $data;
+        return $result;
     }
 }
+
+
