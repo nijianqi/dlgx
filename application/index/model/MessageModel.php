cf8be5ec0479999496b651af77346d6900cc1c65
nijianqi <1198952901@qq.com>
2017-01-11 16:45:26 +0800

    修改发送公众号消息

--- application/index/model/MessageModel.php
+++ application/index/model/MessageModel.php
@@ -18,7 +18,9 @@ class MessageModel extends BaseModel
         $param['comment_id'] = $comment_id;
         $param['create_time'] = time();
         $flag = $this->insertGetId($param);
-        if(!empty($flag)){
+		$likestr = strstr($comment_content,"赞了你的话题");
+		$comlikestr = strstr($comment_content,"赞了你的评论");
+        if(!empty($flag)&&empty($likestr)&&empty($comlikestr)){
             $memberModel = new MemberModel();
             $memberInfo = $memberModel->getInfoById($toMemberId);
             $appId = 'wxd53d2b1ef188dca7';//大乐个学
@@ -38,7 +40,8 @@ class MessageModel extends BaseModel
                     'remark' => array('value' => $message_content),
                 )
             );
-            $res = $this->http_curl($url,'post','json',json_encode($data));
+           // $this->http_curl($url,'post','json',json_encode($data));
+		   $this->curlPost($url,json_encode($data),'POST');
         }
     }
     function get_url_contents($url)
@@ -65,6 +68,31 @@ class MessageModel extends BaseModel
             }
         }
     }
+	function curlPost($url,$data,$method){  
+    $ch = curl_init();   //1.初始化  
+    curl_setopt($ch, CURLOPT_URL, $url); //2.请求地址  
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);//3.请求方式  
+    //4.参数如下  
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);//https  
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);  
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 5.01; Windows NT 5.0)');//模拟浏览器  
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);  
+    curl_setopt($ch, CURLOPT_AUTOREFERER, 1);  
+        curl_setopt($ch, CURLOPT_HTTPHEADER,array('Accept-Encoding: gzip, deflate'));//gzip解压内容  
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');  
+      
+    if($method=="POST"){//5.post方式的时候添加数据  
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);  
+    }  
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+    $tmpInfo = curl_exec($ch);//6.执行  
+  
+    if (curl_errno($ch)) {//7.如果出错  
+        return curl_error($ch);  
+    }  
+    curl_close($ch);//8.关闭  
+    return $tmpInfo;  
+} 
 }
 
 
